035e1bf38a9fc71395301b9a39a5f7bbb395ef6f
MartheNashaat <48573549+MartheNashaat@users.noreply.github.com>
2020-10-09 17:26:36 +0200

    Added orders to profile (not complete)

--- ecommerce/resources/views/profile/show.blade.php
+++ ecommerce/resources/views/profile/show.blade.php
@@ -30,12 +30,49 @@
             <div class="mt-10 sm:mt-0">
                 @livewire('profile.logout-other-browser-sessions-form')
             </div>
-
-            <x-jet-section-border />
-
-            <div class="mt-10 sm:mt-0">
-                @livewire('profile.delete-user-form')
+<br>
+            <div>
+                 <div class="border-t border-gray-200"></div>
             </div>
+    <br>
+            <div >
+                 <label style=" font-size: 115%;">My Orders </label> <br>
+                 <label style="color:#4b5563; font-size: 90%;">Display my previous orders here </label>
+
+                 
+        <div class="mt-5 md:mt-0 md:col-span-2">
+        <div class="px-4 py-5 sm:p-6 bg-white shadow sm:rounded-lg" style ="margin-left:34.5%; width:65%;">
+         
+        
+           <!--  @extends('layouts.search')
+             @section('content')
+              @endsection  -->
+              <div> 
+                  <?php
+                   $email = auth()->user()->email; 
+                   $name = auth()->user()->name;
+                   $date= auth()->user()->created_at;
+                 
+                   $roles = DB::table('users')->where('email', $email)->pluck('email');
+                   foreach ($roles as $email => $title) {
+                    echo '';//$title;
+                }
+                    ?>
+                     <div><pre>{{  $title   }}              Ordered at: {{ $date}}</div>
+                
+             
+             </div> 
+             </div>
         </div>
     </div>
+    
+    <x-jet-section-border />
+
+<div class="mt-10 sm:mt-0">
+    @livewire('profile.delete-user-form')
+</div>
+<br>
+    </div>
+        </div>
+    
 </x-app-layout>
